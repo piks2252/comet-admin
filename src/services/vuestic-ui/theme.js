24f73ff13443295eb674c49e0de7d5268ba384d8
Ajay M <ajay@shivy.co.in>
2021-05-01 19:25:22 +0530

    Changed color

--- src/services/vuestic-ui/theme.js
+++ src/services/vuestic-ui/theme.js
@@ -1,17 +1,17 @@
 export default Object.freeze({
   context: {
     gradient: true,
-    shadow: "lg", // 3 states: 'sm', 'lg', undefined (no shadow).
-    invertedColor: false
+    shadow: 'lg', // 3 states: 'sm', 'lg', undefined (no shadow).
+    invertedColor: false,
   },
   colors: {
-    primary: "#F06767",
-    secondary: "#ECC444",
-    success: "#0C9F67",
-    info: "#007AFF",
-    danger: "#E2376B",
-    warning: "#D48418",
-    gray: "#5B656C",
-    dark: "#000"
-  }
+    primary: '#F06767',
+    secondary: '#E6D656',
+    success: '#0C9F67',
+    info: '#007AFF',
+    danger: '#E2376B',
+    warning: '#D48418',
+    gray: '#5B656C',
+    dark: '#000',
+  },
 });
