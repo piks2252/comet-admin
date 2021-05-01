f337b6506e04224f7c2872c1889e0218f783bbc5
Ajay M <ajay@shivy.co.in>
2021-05-01 18:49:28 +0530

    Inital commit fix

---
+++ src/components/admin/app-navbar/components/AppNavbarActions.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="app-navbar-actions">
+    <notification-dropdown class="app-navbar-actions__item" />
+    <profile-dropdown
+      class="app-navbar-actions__item app-navbar-actions__item--profile"
+    >
+      <span>{{ userName }}</span>
+    </profile-dropdown>
+    <color-dropdown class="app-navbar-actions__item" />
+  </div>
+</template>
+
+<script>
+import ProfileDropdown from "./dropdowns/ProfileDropdown";
+import NotificationDropdown from "./dropdowns/NotificationDropdown";
+import ColorDropdown from "./dropdowns/ColorDropdown";
+import { ColorThemeMixin } from "../../../../services/vuestic-ui";
+
+export default {
+  name: "app-navbar-actions",
+  mixins: [ColorThemeMixin],
+  inject: ["contextConfig"],
+  components: {
+    ColorDropdown,
+    NotificationDropdown,
+    ProfileDropdown
+  },
+  props: {
+    userName: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.app-navbar-actions {
+  display: flex;
+
+  &__item {
+    margin-top: 0.3rem;
+    padding: 0;
+    margin-left: 1.25rem;
+    margin-right: 1.25rem;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+
+    &--profile {
+      display: flex;
+      justify-content: center;
+      margin: auto 0 auto 1.25rem;
+    }
+
+    @include media-breakpoint-down(lg) {
+      margin-right: 0.25rem;
+    }
+
+    @include media-breakpoint-down(sm) {
+      margin-right: 0;
+
+      &:first-of-type {
+        margin-left: 0;
+      }
+
+      &--profile {
+        position: absolute;
+        right: 0.75rem;
+        top: 1.25rem;
+        height: fit-content;
+        margin: auto;
+      }
+    }
+  }
+}
+</style>
---
+++ src/services/vuestic-ui/theme.js
@@ -0,0 +1,17 @@
+export default Object.freeze({
+  context: {
+    gradient: true,
+    shadow: "lg", // 3 states: 'sm', 'lg', undefined (no shadow).
+    invertedColor: false
+  },
+  colors: {
+    primary: "#F06767",
+    secondary: "#ECC444",
+    success: "#0C9F67",
+    info: "#007AFF",
+    danger: "#E2376B",
+    warning: "#D48418",
+    gray: "#5B656C",
+    dark: "#000"
+  }
+});
