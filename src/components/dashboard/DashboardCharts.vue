47dccee54ecc5cbd73c2b0c53eba840b9f97e650
Ajay M <ajay@shivy.co.in>
2021-05-01 19:52:31 +0530

    COverted donut chart to mangas

--- src/components/dashboard/DashboardCharts.vue
+++ src/components/dashboard/DashboardCharts.vue
@@ -12,12 +12,17 @@
         >
           {{ $t('dashboard.charts.showInMoreDetail') }}
         </va-button>
-        <va-chart class="chart" ref="lineChart" :data="lineChartData" type="line"/>
+        <va-chart
+          class="chart"
+          ref="lineChart"
+          :data="lineChartData"
+          type="line"
+        />
       </va-card>
     </div>
 
     <div class="flex xs12 md6 xl3">
-      <va-card :title="$t('dashboard.charts.loadingSpeed')">
+      <va-card title="Mangas">
         <va-button
           icon="fa fa-print"
           flat
@@ -25,74 +30,86 @@
           class="mr-0"
           @click="printChart"
         />
-        <va-chart class="chart chart--donut" :data="donutChartData" type="donut"/>
+        <va-chart
+          class="chart chart--donut"
+          :data="donutChartData"
+          type="donut"
+        />
       </va-card>
     </div>
 
     <div class="flex xs12 md6 xl3">
-      <dashboard-contributors-chart/>
+      <dashboard-contributors-chart />
     </div>
   </div>
 </template>
 
 <script>
-import { getDonutChartData } from '../../data/charts/DonutChartData'
-import { getLineChartData } from '../../data/charts/LineChartData'
-import DashboardContributorsChart from './DashboardContributorsList'
+import { getDonutChartData } from '../../data/charts/DonutChartData';
+import { getLineChartData } from '../../data/charts/LineChartData';
+import DashboardContributorsChart from './DashboardContributorsList';
 
 export default {
   name: 'dashboard-charts',
   components: { DashboardContributorsChart },
-  data () {
+  data() {
     return {
       lineChartData: getLineChartData(this.$themes),
       donutChartData: getDonutChartData(this.$themes),
       lineChartFirstMonthIndex: 0,
-    }
+    };
+  },
+  mounted() {
+    console.log(this.donutChartData);
   },
   watch: {
-    '$themes.primary' () {
-      this.lineChartData = getLineChartData(this.$themes)
-      this.donutChartData = getDonutChartData(this.$themes)
+    '$themes.primary'() {
+      this.lineChartData = getLineChartData(this.$themes);
+      this.donutChartData = getDonutChartData(this.$themes);
     },
 
-    '$themes.info' () {
-      this.lineChartData = getLineChartData(this.$themes)
-      this.donutChartData = getDonutChartData(this.$themes)
+    '$themes.info'() {
+      this.lineChartData = getLineChartData(this.$themes);
+      this.donutChartData = getDonutChartData(this.$themes);
     },
 
-    '$themes.danger' () {
-      this.donutChartData = getDonutChartData(this.$themes)
+    '$themes.danger'() {
+      this.donutChartData = getDonutChartData(this.$themes);
     },
   },
   methods: {
-    deleteSection () {
-      this.lineChartFirstMonthIndex += 1
-      this.lineChartData = getLineChartData(this.$themes, this.lineChartFirstMonthIndex)
-      this.$refs.lineChart.$refs.chart.refresh()
+    deleteSection() {
+      this.lineChartFirstMonthIndex += 1;
+      this.lineChartData = getLineChartData(
+        this.$themes,
+        this.lineChartFirstMonthIndex
+      );
+      this.$refs.lineChart.$refs.chart.refresh();
     },
-    printChart () {
-      const win = window.open('', 'Print', 'height=600,width=800')
-      win.document.write(`<br><img src='${this.donutChartDataURL}'/>`)
+    printChart() {
+      const win = window.open('', 'Print', 'height=600,width=800');
+      win.document.write(`<br><img src='${this.donutChartDataURL}'/>`);
       // TODO: find better solution how to remove timeout
       setTimeout(() => {
-        win.document.close()
-        win.focus()
-        win.print()
-        win.close()
-      }, 200)
+        win.document.close();
+        win.focus();
+        win.print();
+        win.close();
+      }, 200);
     },
   },
   computed: {
-    donutChartDataURL () {
-      return document.querySelector('.chart--donut canvas').toDataURL('image/png')
+    donutChartDataURL() {
+      return document
+        .querySelector('.chart--donut canvas')
+        .toDataURL('image/png');
     },
   },
-}
+};
 </script>
 
 <style scoped>
-  .chart {
-    height: 400px;
-  }
+.chart {
+  height: 400px;
+}
 </style>
--- src/data/charts/DonutChartData.js
+++ src/data/charts/DonutChartData.js
@@ -1,18 +1,24 @@
-let generatedData
+let generatedData;
 
-export const getDonutChartData = (themes) => {
+export const getDonutChartData = themes => {
   if (generatedData) {
-    generatedData.datasets[0].backgroundColor = [themes.danger, themes.info, themes.primary]
+    generatedData.datasets[0].backgroundColor = [
+      themes.danger,
+      themes.info,
+      themes.primary,
+    ];
   } else {
     generatedData = {
-      labels: ['North America', 'South America', 'Australia'],
-      datasets: [{
-        label: 'Population (millions)',
-        backgroundColor: [themes.danger, themes.info, themes.primary],
-        data: [2478, 5267, 734],
-      }],
-    }
+      labels: ['Completed', 'On-going', 'Dropped'],
+      datasets: [
+        {
+          label: 'Mangas (thousands)',
+          backgroundColor: [themes.danger, themes.info, themes.primary],
+          data: [2478, 5267, 734],
+        },
+      ],
+    };
   }
 
-  return generatedData
-}
+  return generatedData;
+};
