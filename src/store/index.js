ffdd8c5f290bd59e562654b27f6408a3555cab86
Ajay M <ajay@shivy.co.in>
2021-06-19 15:17:54 +0530

    Added proper state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,7 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VuexI18n from 'vuex-i18n' // load vuex i18n module
-import app from './modules/app'
+import Vue from 'vue';
+import VuexI18n from 'vuex-i18n'; // load vuex i18n module
+import store from './store';
 
-import * as getters from './getters'
+Vue.use(VuexI18n.plugin, store);
 
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  strict: true, // process.env.NODE_ENV !== 'production',
-  getters,
-  modules: {
-    app,
-  },
-  state: {},
-  mutations: {},
-})
-
-Vue.use(VuexI18n.plugin, store)
-
-export default store
+export default store;
