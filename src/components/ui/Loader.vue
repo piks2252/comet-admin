e433208ba35bce64ee0205edd00b5dc42ac3fc36
Ajay M <ajay@shivy.co.in>
2021-06-24 00:30:38 +0530

    Added loader screen

---
+++ src/components/ui/Loader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="spinners">
+    <div class="flex-center spinner-box">
+      <fulfilling-square-spinner
+        :animation-duration="speed"
+        :color="spinnersColor"
+        :size="size"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { FulfillingSquareSpinner } from 'epic-spinners';
+export default {
+  components: {
+    FulfillingSquareSpinner,
+  },
+  data() {
+    return {
+      size: 80,
+      speed: 1500,
+      spinnersColor: this.$themes.primary,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.spinners {
+  &__size {
+    &-smaller,
+    &-bigger {
+      width: 40px;
+      text-align: center;
+      font-weight: 600;
+    }
+
+    &-smaller {
+      font-size: 1rem;
+    }
+
+    &-bigger {
+      font-size: 1.3rem;
+    }
+  }
+
+  &__duration {
+    &-slower,
+    &-faster {
+      transform: translateY(-1px);
+    }
+  }
+
+  .spinner-box {
+    height: 140px;
+  }
+}
+</style>
