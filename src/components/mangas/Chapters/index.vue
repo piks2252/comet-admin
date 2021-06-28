475a0603cf774176477d367ffb8ffed837e9031e
Ajay M <ajay@shivy.co.in>
2021-06-28 15:13:50 +0530

    added base chapter table

--- src/components/mangas/Chapters/index.vue
+++ src/components/mangas/Chapters/index.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>{{ mangaId }}</div>
+  <div>
+    <div class="row align--center">
+      <div class="flex xs12 offset--md8 offset--lg7">
+        <va-button color="success">Update chapter list</va-button>
+        <va-button>Add Chapter</va-button>
+      </div>
+    </div>
+    <div class="row align--center">
+      <div class="flex md10 lg11">
+        <loader v-if="apiLoading" />
+        <div v-else>
+          <h1>Chapters</h1>
+          <draggable v-model="chapters" style="margin-top: 10px;">
+            <transition-group>
+              <chapter-row
+                v-for="chapter in chapters"
+                :key="chapter.id"
+                :chapter="chapter"
+              />
+            </transition-group>
+          </draggable>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable';
 import Loader from '../../ui/Loader';
+import ChapterRow from './ChapterRow';
 import { fetchChapters } from '../../../apollo/api/mangas';
 
 export default {
-  components: { Loader },
+  components: { Loader, draggable, ChapterRow },
   props: {
     mangaId: {
       type: String,
@@ -19,9 +45,13 @@ export default {
       chapters: [],
     };
   },
+  async mounted() {
+    if (this.mangaId) {
+      await this.loadChapters();
+    }
+  },
   methods: {
     async loadChapters() {
-      // console.log('making reqwuest to ', this.mangaId);
       this.apiLoading = true;
       try {
         const { chaptersList } = await fetchChapters(this.mangaId);
@@ -34,6 +64,7 @@ export default {
           fullWidth: false,
         });
       }
+      this.apiLoading = false;
     },
   },
 };
