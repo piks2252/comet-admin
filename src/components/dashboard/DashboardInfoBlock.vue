d9192cb2b2333def3724d91d41358245d89fb066
Ajay M <ajay@shivy.co.in>
2021-07-06 19:01:33 +0530

    Made some minor changes

--- src/components/dashboard/DashboardInfoBlock.vue
+++ src/components/dashboard/DashboardInfoBlock.vue
@@ -1,45 +1,7 @@
 <template>
   <div class="row row-equal">
-    <div class="flex xl6 xs12">
-      <div class="row">
-        <div class="flex xs12 sm4">
-          <va-card class="mb-4" color="success">
-            <p class="display-2 mb-0" style="color: white;">
-              {{ overAllStats.users }}
-            </p>
-            <p>Users</p>
-          </va-card>
-        </div>
-        <div class="flex xs12 sm4">
-          <va-card class="mb-4" color="info">
-            <p class="display-2 mb-0" style="color: white;">
-              {{ overAllStats.devices }}
-            </p>
-            <p>Devices</p>
-          </va-card>
-        </div>
-        <div class="flex xs12 sm4">
-          <va-card class="mb-4" color="danger">
-            <p class="display-2 mb-0" style="color: white;">
-              {{ overAllStats.issues }}
-            </p>
-            <p>Issues</p>
-          </va-card>
-        </div>
-      </div>
-    </div>
     <div class="flex xs12 xl6">
       <div class="row">
-        <div class="flex xs12 md6">
-          <va-card>
-            <p class="display-2 mb-1" :style="{ color: this.$themes.primary }">
-              {{ totalMangas }}
-            </p>
-            <p class="no-wrap">
-              Mangas
-            </p>
-          </va-card>
-        </div>
         <div class="flex xs12 md6">
           <va-card>
             <div class="row row-separated">
@@ -57,7 +19,7 @@
                   users
                 </p>
               </div>
-              <div class="flex xs4">
+              <div class="flex xs6">
                 <p
                   class="display-2 mb-1 text--center"
                   :style="{ color: this.$themes.info }"
@@ -65,21 +27,55 @@
                   {{ todayStats.chapters }}
                 </p>
                 <p class="text--center no-wrap mb-1">
-                  chapters
-                </p>
-              </div>
-              <div class="flex xs4">
-                <p
-                  class="display-2 mb-1 text--center"
-                  :style="{ color: this.$themes.warning }"
-                >
-                  {{ todayStats.downloads }}
-                </p>
-                <p class="text--center mb-1">
-                  downloads
+                  chapters scraped
                 </p>
               </div>
             </div>
+          </va-card>
+        </div>
+        <div class="flex xs12 md6">
+          <va-card>
+            <p class="display-2 mb-1" :style="{ color: this.$themes.primary }">
+              {{ totalMangas }}
+            </p>
+            <p class="display-4 mb-1" :style="{ color: this.$themes.info }">
+              {{ 0 }} added today
+            </p>
+            <p class="no-wrap">
+              Mangas
+            </p>
+          </va-card>
+        </div>
+      </div>
+    </div>
+    <div class="flex xl6 xs12">
+      <div class="flex row">
+        <div class="flex xs12 sm3">
+          <va-card class="mb-3" color="success">
+            <p class="display-2 mb-0" style="color: white;">
+              {{ overAllStats.users }}
+            </p>
+            <p>Users</p>
+          </va-card>
+        </div>
+        <div class="flex xs12 sm5">
+          <va-card class="mb-5" color="info">
+            <p class="display-2 mb-0" style="color: white;">
+              {{ overAllStats.devices }}
+            </p>
+            <p>
+              Android: 1 | iOS: 12
+            </p>
+            <p>devices</p>
+          </va-card>
+        </div>
+        <div class="flex xs12 sm4">
+          <va-card class="mb-4" color="danger">
+            <p class="display-2 mb-0" style="color: white;">
+              {{ overAllStats.issues }}
+            </p>
+            <p style="color: yellow;">Closed: 754</p>
+            <p>issues</p>
           </va-card>
         </div>
       </div>
