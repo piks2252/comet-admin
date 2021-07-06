bc41faebe3ae11a2eff48ecb39c49b830a5c92fe
Ajay M <ajay@shivy.co.in>
2021-07-06 13:27:28 +0530

    Added chapter row

---
+++ src/components/mangas/Chapters/ChapterRow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="chapter-row">
+    <p scope="row">Volume: {{ chapter.volume }}</p>
+    <p>Chapter: {{ chapter.chapter }}</p>
+    <p>Title: {{ chapter.title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    chapter: {
+      type: Object,
+    },
+  },
+};
+</script>
+<style lang="scss">
+.chapter-row {
+  border: 1px solid #e8e5e5;
+  padding: 8px;
+
+  &:focus {
+    border: 1px dashed #e8e5e5;
+  }
+}
+</style>
