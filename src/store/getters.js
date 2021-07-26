64c2de12317be31dffa9e9f00b0141741cf52619
Ajay M <ajay@shivy.co.in>
2021-07-27 01:17:06 +0530

    Using manga for selected items

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,4 +4,21 @@ const isLoading = state => state.app.isLoading;
 const backgroundLoading = state => state.app.backgroundLoading;
 const currentPage = state => state.app.currentPage;
 
-export { config, palette, isLoading, backgroundLoading, currentPage };
+const selectedMangaId = state => state.manga.selectedManga.id;
+const isMangaSaved = state => state.manga.selectedManga.isSaved;
+const selectedChapterId = state => state.manga.selectedChapter.id;
+const isChapterSaved = state => state.manga.selectedChapter.isSaved;
+const isPagesSaved = state => state.manga.selectedChapter.pagesSaved;
+
+export {
+  config,
+  palette,
+  isLoading,
+  backgroundLoading,
+  currentPage,
+  selectedMangaId,
+  isMangaSaved,
+  selectedChapterId,
+  isChapterSaved,
+  isPagesSaved,
+};
--- src/store/modules/manga.js
+++ src/store/modules/manga.js
@@ -1,9 +1,32 @@
 const state = {
-  selectedManga: {},
-  selectedChapter: {},
+  selectedManga: {
+    id: null,
+    isSaved: true,
+  },
+  selectedChapter: {
+    id: null,
+    isSaved: true,
+    pagesSaved: true,
+  },
 };
 
-const mutations = {};
+const mutations = {
+  setMangaId(state, mangaId) {
+    state.selectedManga.id = mangaId;
+  },
+  setMangaSave(state, isSaved) {
+    state.isSaved = isSaved;
+  },
+  setChapterId(state, chapterId) {
+    state.selectedChapter.id = chapterId;
+  },
+  setChapterSave(state, isSaved) {
+    state.selectedChapter.isSaved = isSaved;
+  },
+  setChapterPageSave(state, isSaved) {
+    state.selectedChapter.pagesSaved = isSaved;
+  },
+};
 
 const actions = {};
 
